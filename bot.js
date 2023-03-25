2ac9c4fd91d477d1a31e2c30fc39af37935c7fee
NoM84 <126786961+NoM84@users.noreply.github.com>
2023-03-26 01:34:36 +0300

    an update for regular

--- bot.js
+++ bot.js
@@ -17,6 +17,7 @@ client.on('ready',() =>{
     client.user.setActivity(`Abd ye selamlar`, { tpye: "STREAMING"})
 });
 
+var kontrol = 0;
 
 
 
@@ -37,54 +38,103 @@ client.on('ready',() =>{
 
 
 client.on('messageCreate',message =>{
-    if(message.content == 'ping'){
-        message.reply("pong")
+    if(message.author.bot) return;
+    if((message.content.toLowerCase() == "agamod aç")&&(message.author.id == '718497288963620904')) {
+        kontrol = 1;
+        message.reply("Aga mod Açıldı")
     }
-    if(message.content =="ehehe"){
-        message.reply("Hahaha")
+    if((message.content.toLowerCase() == "agamod kapa")&&(message.author.id == '718497288963620904')) {
+        kontrol =0;
+        message.reply("Aga Mod Kapandı")
     }
-    if((message.content.includes("Çağrı"))||(message.content.includes("çağrı"))){
-        message.react("🇬")
-        message.react('🇹')
-        message.react("🇪")
-        message.react("🇳")
+    
+    if(kontrol == 0) {
+        if(message.content == 'ping'){
+            message.reply("pong")
+        }
+
+        if(message.content.toLowerCase() == "!komutlar") {
+            message.author.send("----------------------------------------------\nsa içeren her kelime");
+            message.author.send("ping");
+        }
+
     }
-    if((message.content == "billurunu kemireyim")||(message.content == "Billurunu kemireyim")){
-        message.reply("Olur")
+    
+    
+    
+    
+    
+    
+    
+    
+    if(kontrol == 1) {
+        if(message.content.toLowerCase() == "!komutlar") {
+            message.author.send("sa içeren her kelime");
+            message.author.send("çağrı (içeren her kelime)");
+            message.author.send("billurunu kemireyim (=) ");
+            message.author.send("hanifi (içeren her kelime)");
+            message.author.send("iki tane ince içeren her kelime");
+            message.author.send("naber aga (=)");
+            message.author.send("sik kelimesi içeren her cümle");
+            message.author.send("aga içeren her cümle");
+            message.author.send("biraz nostalji göster (=)");
+
+        }
+
+        if(message.content.toLowerCase().includes("çağrı")){
+            message.react("🇬")
+            message.react('🇹')
+            message.react("🇪")
+            message.react("🇳")
+        }
+
+        if(message.content.toLowerCase() == "billurunu kemireyim"){
+            message.reply("Olur")
+        }
+
+        if((message.content.includes("hanifi"))||(message.content.includes("Hanifi"))){
+            message.reply("....");
+            message.channel.send({ files: ['https://i.hizliresim.com/aa7j6a4.png']})
+        }
+        
+        if((message.content.includes("ince"))||(message.content.includes("İnce"))){
+            let kelimesayisi = (message.content.match(new RegExp("İnce","g")) || []).length
+            let kelimesayisi2 = (message.content.match(new RegExp("ince","g")) || []).length
+            if ((kelimesayisi==2)||(kelimesayisi2==2)||((kelimesayisi==1)&&(kelimesayisi2==1))){
+                message.channel.send({ files: ['https://i.hizliresim.com/jhystk8.jpg']})
+            }
+        }
+
+        if(message.content.toLowerCase() == "naber aga") {
+            message.reply(`İyiyim Senden naber ${message.member.displayName}`)
+        }
+
+        if((message.content.includes("sik"))){
+            message.reply("Lütfen Küfür etmeyelim")
+        }
+
+        if(message.content.toLowerCase() == "aga"){
+            message.reply("Efendim")
+        }
+
+        if(message.content.toLowerCase() == "biraz nostalji göster"){
+            message.reply("Tamam")
+            message.channel.send({ files: ['https://i.hizliresim.com/f8k08tw.jpg']}) //1
+            message.channel.send({ files: ['https://i.hizliresim.com/qowv0x8.jpg']}) //2
+            message.channel.send({ files: ['https://i.hizliresim.com/fic02bc.jpg']}) //3
+            message.channel.send({ files: ['https://i.hizliresim.com/3ea6wpc.jpg']}) //4
+            message.channel.send({ files: ['https://i.hizliresim.com/hvt4rkz.jpg']}) //5
+            message.channel.send({ files: ['https://i.hizliresim.com/cz2c8p2.jpg']}) //6
+            message.channel.send({ files: ['https://i.hizliresim.com/9w5dnre.jpg']}) //7
+        }
+
+
+
     }
+    
     if(message.content.toLowerCase() == 'sa'){
         message.react("🇦")
         message.react("🇸")
-    }
-    if((message.content.includes("hanifi"))||(message.content.includes("Hanifi"))){
-        message.reply("....");
-        message.channel.send({ files: ['https://i.hizliresim.com/aa7j6a4.png']})
-    }
-    if((message.content.includes("ince"))||(message.content.includes("İnce"))){
-        let kelimesayisi = (message.content.match(new RegExp("İnce","g")) || []).length
-        let kelimesayisi2 = (message.content.match(new RegExp("ince","g")) || []).length
-        if ((kelimesayisi==2)||(kelimesayisi2==2)||((kelimesayisi==1)&&(kelimesayisi2==1))){
-            message.channel.send({ files: ['https://i.hizliresim.com/jhystk8.jpg']})
-        }
-    }
-    if((message.content == "Naber aga")||(message.content == "Naber Aga")||(message.content == "naber aga")||(message.content == "naber Aga")){
-        message.reply(`İyiyim Senden naber ${message.member.displayName}`)
-    }
-    if((message.content.includes("sik"))){
-        message.reply("Lütfen Küfür etmeyelim")
-    }
-    if(message.content.toLowerCase() == "aga"){
-        message.reply("Efendim")
-    }
-    if(message.content.toLowerCase() == "biraz nostalji göster"){
-        message.reply("Tamam")
-        message.channel.send({ files: ['https://i.hizliresim.com/f8k08tw.jpg']}) //1
-        message.channel.send({ files: ['https://i.hizliresim.com/qowv0x8.jpg']}) //2
-        message.channel.send({ files: ['https://i.hizliresim.com/fic02bc.jpg']}) //3
-        message.channel.send({ files: ['https://i.hizliresim.com/3ea6wpc.jpg']}) //4
-        message.channel.send({ files: ['https://i.hizliresim.com/hvt4rkz.jpg']}) //5
-        message.channel.send({ files: ['https://i.hizliresim.com/cz2c8p2.jpg']}) //6
-        message.channel.send({ files: ['https://i.hizliresim.com/9w5dnre.jpg']}) //7
     }
     
 
